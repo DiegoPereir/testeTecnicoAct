37dfd44c27add113411f1797a17e0ff7b409d533
DiegoCogna <diego.pereira-all@cogna.parceirosedu.com.br>
2025-07-30 18:34:41 -0300

    chore: ajustes do realizar busca

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,7 +4,7 @@ Cypress.Commands.add('realizarBusca', (texto) => {
         if ($field.is(':visible')) {
             cy.wrap($field).type(`${texto} {enter}`)
         } else {
-            cy.wait(1000)
+            cy.wait(2000)
             cy.get('[aria-label="Search button"]').first().click()
             cy.get('#search-field').should('be.visible').type(`${texto} {enter}`)
         }
