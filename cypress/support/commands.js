4b979058385a87c405fc1dc59d11b3e730f1a3ba
DiegoCogna <diego.pereira-all@cogna.parceirosedu.com.br>
2025-07-30 12:44:23 -0300

    chore: ajustes no carregamento da pagina

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,32 +10,9 @@ Cypress.Commands.add('validarRedirecionamento', (caminhoUrl, H1presente) => {
 })
 
 Cypress.Commands.add('aguardarPaginaCarregar', () => {
-    cy.fixture('mockRequestCdn').then((mockRequestCdn) => {
-        cy.intercept(
-            'GET',
-            '**/amp-story.pt-BR.json*',
-            (req) => {
-                req.reply({
-                    statusCode: 200,
-                    body: mockRequestCdn,
-                    delay: 100,
-                });
-            }
-        ).as('getAmpStory');
-
-        cy.visit('/');
-
-        // Força a requisição para ativar o intercept
-        cy.request({
-            method: 'GET',
-            url: 'https://cdn.ampproject.org/rtv/9999/v0/amp-story.pt-BR.json?__amp_source_origin=https%3A%2F%2Fblog.agibank.com.br',
-            failOnStatusCode: false 
-        });
-
-        cy.wait('@getAmpStory', { timeout: 10000 })
-            .its('response.statusCode')
-            .should('eq', 200);
-    });
+    cy.visit('/');
+    cy.get('[viewBox="0 0 256 512"]').should('be.visible');
 });
+
 
 
