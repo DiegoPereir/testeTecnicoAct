0e3b5adf214e7320e48bfdc4eff784f9826c3517
DiegoCogna <diego.pereira-all@cogna.parceirosedu.com.br>
2025-07-30 12:17:07 -0300

    chore: adicionando estrutura inicial

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  video: true,
+  retries: 1,
+  viewportWidth: 1600,
+  viewportHeight: 900,
+  e2e: {
+    baseUrl: 'https://blog.agibank.com.br',
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    }
+  },
+});
