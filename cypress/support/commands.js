4f9fc57e49172e8f8ff0db7023cdbc244618329b
DiegoCogna <diego.pereira-all@cogna.parceirosedu.com.br>
2025-07-30 15:20:26 -0300

    chore: ajustes do realizar busca

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,16 @@
 Cypress.Commands.add('realizarBusca', (texto) => {
-    cy.get('[aria-label="Search button"]').first().click({force: true})
+    cy.get('[aria-label="Search button"]').first().click()
     cy.get('#search-field').type(`${texto} {enter}`)
     cy.contains(`Resultados encontrados para: ${texto}`).should('be.visible')
 })
 
 Cypress.Commands.add('validarRedirecionamento', (caminhoUrl, H1presente) => {
-    cy.url().should('include', caminhoUrl);
-    cy.get('h1').should('contain.text', H1presente);
+    cy.url().should('include', caminhoUrl)
+    cy.get('h1').should('contain.text', H1presente)
 })
 
 Cypress.Commands.add('aguardarPaginaCarregar', () => {
-    cy.intercept('POST', '**/wp-content/plugins/litespeed-cache/guest.vary.php').as('litespeed');
-    cy.visit('/institucional/');
-    cy.wait('@litespeed').its('response.statusCode').should('eq', 200);
-    cy.url().should('include', '/institucional/');
+    cy.intercept('POST', '**/wp-content/plugins/litespeed-cache/guest.vary.php').as('litespeed')
+    cy.visit('/')
+    cy.wait('@litespeed').wait(1000).its('response.statusCode').should('eq', 200)
 });
