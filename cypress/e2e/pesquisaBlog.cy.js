f3acc575bd955d05be855e5d028a5fef801f69d2
DiegoCogna <diego.pereira-all@cogna.parceirosedu.com.br>
2025-07-30 19:08:25 -0300

    chore: ajustes do realizar busca

--- cypress/e2e/pesquisaBlog.cy.js
+++ cypress/e2e/pesquisaBlog.cy.js
@@ -1,7 +1,7 @@
 describe('Testes da funcionalidade pesquisa', () => {
    beforeEach(function () {
       cy.fixture("dados").as('dadosAgibank');
-      cy.visit('/')
+      cy.visit('/servicos')
    });
 
    it('Realizar busca com sucesso', function () {
