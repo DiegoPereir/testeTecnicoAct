b53f22bf361c301d2788c9ce6d3fea6b41821deb
DiegoCogna <diego.pereira-all@cogna.parceirosedu.com.br>
2025-07-30 17:26:36 -0300

    chore: ajustes do realizar busca

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 5000,
+  defaultCommandTimeout: 10000,
   video: true,
   retries: 2,
   viewportWidth: 1600,
